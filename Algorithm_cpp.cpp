7ea4d809ae2076904ed220389584d8d7dc65e97d
Lee Jae-Baek <103738589+ThinkingVincent@users.noreply.github.com>
2022-10-09 15:35:24 +0900

    돌림판 문자열 (25705)
    INPC에 제출하여 정답 처리됐던 코드

--- Algorithm_cpp.cpp
+++ Algorithm_cpp.cpp
@@ -1,8 +1,46 @@
 ﻿#include <iostream>
+#include <string>
 
 using namespace std;
 
 int main()
 {
+	int n, m;
+	string a, b;
+	int count = 1;
+	int flag = 1;
+	cin >> n;
+	cin >> a;
+	cin >> m;
+	cin >> b;
 
+	char temp = a[n - 1];
+	string c;
+	c.push_back(temp);
+	for (int i = 0; i < n - 1; ++i)
+	{
+		c.push_back(a[i]);
+	}
+
+	for (int i = 0; i < m; ++i)
+	{
+		while (true)
+		{
+			if (c[count % n] == b[i])
+			{
+				++count;
+				break;
+			}
+			else ++count;
+			if (count > 10001)
+			{
+				flag = 0;
+				break;
+			}
+		}
+	}
+	if (flag == 0)
+		cout << -1;
+	else
+		cout << count - 1;
 }
